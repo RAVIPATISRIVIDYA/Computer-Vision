c188e97be87e8ebf27afbf987597922f31ff84cb
RAVIPATISRIVIDYA <51819923+RAVIPATISRIVIDYA@users.noreply.github.com>
2022-05-01 14:50:28 -0400

    Update face_recognizer.py

--- Assignment3_PartC/face_recognizer.py
+++ Assignment3_PartC/face_recognizer.py
@@ -7,13 +7,10 @@ face_cascade_Path = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
-# names related to ids: The names associated to the ids: 1 for Mohamed, 2 for Jack, etc...
-names = ['None'] # add a name into this list
-#Video Capture
+names = ['None']
 cam = cv2.VideoCapture(0)
 cam.set(3, 640)
 cam.set(4, 480)
-# Min Height and Width for the  window size to be recognized as a face
 minW = 0.1 * cam.get(3)
 minH = 0.1 * cam.get(4)
 while True:
@@ -34,7 +31,6 @@ while True:
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
         else:
-            # Unknown Face
             id = "Who are you ?"
             confidence = "  {0}%".format(round(100 - confidence))
 
